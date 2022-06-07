109acbe6c2d08b350318af1581aa511e8af7d550
Tanmay000009 <tanmayvyas009@gmail.com>
2022-06-07 22:41:35 +0530

    create navbar and add static files

---
+++ components/navbar.js
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import styles from "../styles/components/Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand navbar-light navbar-bg">
+        <Link href="/" className={styles.sidebarToggle}>
+          <a>
+            <Image
+              src="/img/sglogo.png"
+              height={42}
+              width={70}
+              className={styles.logo}
+              alt="StudyHub"
+            />
+          </a>
+        </Link>
+        <div className="navbar-collapse collapse">
+          <ul className="navbar-nav navbar-align">
+            <div className="search-box">
+              <form
+                className={styles.searchForm}
+                action="/courses/search"
+                method="POST"
+              >
+                <div
+                  className={`input-group autocomplete ${styles.searchNavbar}`}
+                >
+                  <input
+                    type="text"
+                    className="form-control search-input"
+                    placeholder="Search Course"
+                    aria-label="Search"
+                    name="query"
+                    id="autoCourse"
+                  />
+                  <div className={styles.searchButton}>
+                    <button>
+                      <i className="fas fa-search"></i>
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,75 @@
 import Head from "next/head";
 import Image from "next/image";
 
+import Navbar from "../components/navbar";
+
 import styles from "../styles/Landing.module.css";
 
 export default function Home() {
   return (
-    <div
-      data-theme="default"
-      data-layout="fluid"
-      data-sidebar-position="left"
-      data-sidebar-behavior="sticky"
-      className={styles.globalStyle}
-    >
-      <div className={styles.googlePlayImage}>
-        <a href="https://play.google.com/store/apps/details?id=com.vinnovateit.studyhub">
-          <Image
-            id="gplaybadge"
-            width={120}
-            height={55}
-            src="/images/google-play-badge.png"
-            alt="App Link"
-          />
-        </a>
-      </div>
-      <div className={styles.wrapper}>
-        <div className={styles.main}>
-          <header className={styles.header}>
-            <div className={styles.content}>
-              <h1 className={styles.h1}>
-                StudyHub by
-                <span className={styles.viit}></span> VinnovateIT
-              </h1>
-              <p className={`${styles.revcolor} ${styles.landingp}`}>
-                <span className={styles.quotes}> {'"'} </span>
-                {
-                  "A 'CAT'ing chai and StudyHub from VinnovateIT is all you need for your exams!!"
-                }
-                <span className={styles.quotes - 2}>{' "'} </span>
-              </p>
-              <h4 className={styles.desc}>
-                No need to worry about your exams anymore, we present to you the
-                StudyHub. All in one resource collection for VIT
-              </h4>
+    <>
+      <Navbar />
+      <div
+        data-theme="default"
+        data-layout="fluid"
+        data-sidebar-position="left"
+        data-sidebar-behavior="sticky"
+        className={styles.globalStyle}
+      >
+        <div className={styles.googlePlayImage}>
+          <a href="https://play.google.com/store/apps/details?id=com.vinnovateit.studyhub">
+            <Image
+              id="gplaybadge"
+              width={120}
+              height={55}
+              src="/images/google-play-badge.png"
+              alt="App Link"
+            />
+          </a>
+        </div>
+        <div className={styles.wrapper}>
+          <div className={styles.main}>
+            <header className={styles.header}>
+              <div className={styles.content}>
+                <h1 className={styles.h1}>
+                  StudyHub by
+                  <span className={styles.viit}></span> VinnovateIT
+                </h1>
+                <p className={`${styles.revcolor} ${styles.landingp} contentP`}>
+                  <span className={styles.quotes}> {'"'} </span>
+                  {
+                    "A 'CAT'ing chai and StudyHub from VinnovateIT is all you need for your exams!!"
+                  }
+                  <span className={styles.quotes - 2}>{' "'} </span>
+                </p>
+                <h4 className={styles.desc}>
+                  No need to worry about your exams anymore, we present to you
+                  the StudyHub. All in one resource collection for VIT
+                </h4>
 
-              <div className={styles.buttons}>
-                <a href="branch/firstsem">1st Sem</a>
-                <a href="branch/it">IT</a>
-                <a href="branch/cse">CSE</a>
-                <a href="branch/eee">EEE</a>
-                <a href="branch/mech">MECH</a>
-                <a href="branch/ece">ECE</a>
-                <a href="branch/uc">U.C.</a>
+                <div className={styles.buttons}>
+                  <a href="branch/firstsem">1st Sem</a>
+                  <a href="branch/it">IT</a>
+                  <a href="branch/cse">CSE</a>
+                  <a href="branch/eee">EEE</a>
+                  <a href="branch/mech">MECH</a>
+                  <a href="branch/ece">ECE</a>
+                  <a href="branch/uc">U.C.</a>
+                </div>
               </div>
-            </div>
-            <div className={styles.imgBox}>
-              <Image
-                src="/images/studyguide.svg"
-                height="400px"
-                width="550px"
-                alt=""
-              />
-            </div>
-          </header>
+
+              <div className={styles.imgBox}>
+                <Image
+                  src="/images/studyguide.svg"
+                  height="400px"
+                  width="550px"
+                  alt=""
+                />
+              </div>
+            </header>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
