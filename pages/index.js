49c1b633d2d62d8a16fcab3b565240cf470abf88
Tanmay000009 <tanmayvyas009@gmail.com>
2022-06-14 14:56:37 +0530

    remove unecessary console logs

--- pages/index.js
+++ pages/index.js
@@ -85,7 +85,6 @@ export async function getServerSideProps() {
     if (mongoose.connections[0].readyState) {
       //Execute fetch here
     } else {
-      console.log(process.env.MONGO_URI);
       await mongoose
         .connect(process.env.MONGO_URI, {
           useUnifiedTopology: true,
