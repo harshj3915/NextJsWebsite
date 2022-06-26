4b92f12fcae4cc24dd31895d5b74c81665736515
Tanmay000009 <tanmayvyas09@gmail.com>
2022-06-26 16:53:58 +0530

    add meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -16,7 +16,63 @@ export default function Document() {
             `,
           }}
         />
+
         <link rel="icon" type="image/png" href="/img/sglogofav.png"></link>
+
+        <meta name="author" content="VinnovateIT" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta
+          name="description"
+          content="The guide for your CATs and FATs - StudyHub"
+        />
+        <meta name="theme-color" content="#17a2b8" />
+        {/* <!-- FOR FACEBOOK --> */}
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="StudyHub - The guide for your CATs and FATs."
+        />
+        <meta
+          property="og:description"
+          content="The best resources and materials to help ace college and university examinations and  challenges."
+        />
+        <meta
+          property="og:image"
+          content="https://studyhub.vinnovateit.com/img/sglogofav.png"
+        />
+        <meta property="og:url" content="https://studyhub.vinnovateit.com/" />
+        <meta property="og:site_name" content="StudyHub" />
+
+        {/* <!-- FOR FACEBOOK END --> */}
+        {/* <!-- FOR TWITTER --> */}
+
+        <meta name="twitter:card" content="website" />
+        <meta name="twitter:site" content="StudyHub" />
+        <meta
+          name="twitter:title"
+          content="StudyHub - The guide for your CATs and FATs."
+        />
+        <meta
+          name="twitter:description"
+          content="The best resources and materials to help ace college and university examinations and  challenges."
+        />
+        <meta name="twitter:creator" content="@Sriesh_Agrawal" />
+        <meta
+          name="twitter:image"
+          content="https://studyhub.vinnovateit.com/img/sglogofav.png"
+        />
+
+        <meta itemProp="name" content="StudyHub" />
+        <meta
+          itemProp="description"
+          content="The best resources and materials to help ace college and university examinations and  challenges."
+        />
+        <meta itemProp="image" content="#" />
+        {/* <!-- FOR TWITTER END  --> */}
 
         <Script
           src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"
